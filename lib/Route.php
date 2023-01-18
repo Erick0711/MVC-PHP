1e84038e3cb41be2f45fbf930bee32a568a383a9
Maicol Erick Arteaga Guzman <88513626+Erick0711@users.noreply.github.com>
2023-01-17 22:09:11 -0400

    SE AGREGO LAS RUTAS QUE SE EJECUTARAN YA SEA GET O POST

---
+++ lib/Route.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Lib;
+
+class Route
+{
+    private static $routes = [];
+
+    public static function get($uri, $callback)
+    {
+        $uri = trim($uri, '/'); // SE ENCARGA DE ELIMINAR EL SLASH / TANTO AL AL PRINSIPIO O AL FINAL /contact = > contact
+        self::$routes['GET'][$uri] = $callback;
+    }
+    public static function post($uri, $callback)
+    {
+        $uri = trim($uri, '/');
+        self::$routes['POST'][$uri] = $callback;
+    }
+
+    public static function dispatch()
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+        $uri = trim($uri, '/');
+
+        $method = $_SERVER['REQUEST_METHOD'];
+        foreach (self::$routes[$method] as $route => $callback) {
+            if($route == $uri){
+                $callback(); // RECUPERA LA FUNCION Y LA EJECUTA
+                return;
+            }
+        }
+        echo '404';
+    }
+}
